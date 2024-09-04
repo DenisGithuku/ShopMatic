f0ee9a9920ee71e228caa0c8924706019703753c
githukudenis <githukudenis@gmail.com>
2024-09-04 15:27:52 +0300

    Initial commit

---
+++ lib/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFF21D96B);
+const Color secondaryColor = Colors.black;
+const Color backgroundColor = Color(0xFFF5F5F5);
+const Color textColor = Colors.black;
