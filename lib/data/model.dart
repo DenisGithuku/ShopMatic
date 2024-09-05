e5d7631f28ba6635a0e5367f2476ab3193cfe628
githukudenis <githukudenis@gmail.com>
2024-09-05 07:13:42 +0300

    Create product model

---
+++ lib/data/model.dart
@@ -0,0 +1,43 @@
+class Product {
+  final String id;
+  final String name;
+  final String description;
+  final double price;
+  final String category;
+  final List<String> tags;
+  final List<Option> options;
+  final List<Variant> variants;
+  final String? imagePath;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.category,
+    required this.tags,
+    required this.options,
+    required this.variants,
+    required this.imagePath,
+  });
+}
+
+class Option {
+  final String name;
+  final List<String> values;
+
+  Option({required this.name, required this.values});
+}
+
+class Variant {
+  final String id;
+  final String name;
+  final double price;
+  final int stock;
+
+  Variant(
+      {required this.id,
+      required this.name,
+      required this.price,
+      required this.stock});
+}
