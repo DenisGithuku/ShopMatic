f638fe7882ad2871b3b0c330863f29ef1f0970bf
githukudenis <githukudenis@gmail.com>
2024-09-09 09:36:05 +0300

    Extract reusable widgets

---
+++ lib/screens/new_product/components/option_chips_section.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:product_variant_gen/screens/new_product/components/add_option_button.dart';
+
+class OptionChipRow extends StatelessWidget {
+  final List<String> values;
+  const OptionChipRow({super.key, required this.values});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Wrap(
+          spacing: 8.0,
+          runSpacing: 4.0,
+          children: values.map(
+            (value) {
+              return Chip(
+                label: Text(value),
+              );
+            },
+          ).toList()),
+    );
+  }
+}
+
+class OptionChipsSection extends StatelessWidget {
+  final Map<String, List<String>> optionsMap;
+  final Function() onAddOption;
+
+  const OptionChipsSection(
+      {super.key, required this.optionsMap, required this.onAddOption});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Column(
+          children: optionsMap.entries.map((entry) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  entry.key,
+                ),
+                OptionChipRow(values: entry.value)
+              ],
+            );
+          }).toList(),
+        ),
+        AddOptionButton(onAddOption: onAddOption)
+      ],
+    );
+  }
+}
---
+++ lib/screens/new_product/components/options_section.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:product_variant_gen/screens/new_product/components/add_option_button.dart';
+import 'package:product_variant_gen/screens/new_product/components/option_value_input.dart';
+import 'package:product_variant_gen/screens/new_product/components/user_input_field.dart';
+
+class OptionsSection extends StatelessWidget {
+  final Map<TextEditingController, List<TextEditingController>>
+      optionControllersMap;
+  final Function(TextEditingController) onRemoveField;
+  final Function() onStoreOption;
+  final Function() onAddOption;
+  final Function() onAddOptionValue;
+
+  const OptionsSection(
+      {super.key,
+      required this.optionControllersMap,
+      required this.onRemoveField,
+      required this.onStoreOption,
+      required this.onAddOption,
+      required this.onAddOptionValue});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Column(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Option name'),
+                Column(
+                  children: [
+                    UserInputField(
+                      controller: optionControllersMap.entries.last.key,
+                      hintText: 'Color',
+                    )
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Option values'),
+                Column(
+                  children:
+                      optionControllersMap.entries.last.value.map((controller) {
+                    return OptionValueInputRow(
+                        controller: controller,
+                        onRemoveField: onRemoveField(controller));
+                  }).toList(),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: OutlinedButton(
+                    onPressed: onAddOptionValue,
+                    child: Text('Add value'),
+                  ),
+                ),
+                SizedBox(
+                  width: 16.0,
+                ),
+                Expanded(
+                  child: OutlinedButton(
+                    onPressed: onStoreOption,
+                    child: Text('Done'),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        AddOptionButton(onAddOption: onAddOption)
+      ],
+    );
+  }
+}
